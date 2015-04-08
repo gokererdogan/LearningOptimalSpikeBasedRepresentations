c991c4f693896e9d93c9ffa4cc811f2e26c8b98f
gerdogan <gokererdogan@gmail.com>
2015-04-08 13:47:34 -0400

    Added comments.

--- dev.m
+++ dev.m
@@ -1,6 +1,8 @@
+% number of timesteps
 n = 100;
 ts = linspace(0, 1, n);
 
+% a simple step function
 x = zeros(n, 1);
 x(30:50) = 0.1;
 x(70:85) = 0.1;
@@ -10,24 +12,38 @@ xp(51) = -1;
 xp(70) = 1;
 xp(86) = -1;
 
-x = sin(ts)./4;
-xp = cos(ts)./4;
+% % sine wave input
+% x = sin(ts)./4;
+% xp = cos(ts)./4;
 
+% current membrane voltage
 v = 0;
+% output weight
 w = 0.1;
+% spike train
 o = zeros(n,1);
+% v(t)
 V = zeros(n,1);
+% xhat(t) (prediction)
 xh = zeros(n,1);
 
+
 for t = 2:n
+    % membrane voltage update equation
     dv = -v + w*(x(t-1) + xp(t-1)) - w^2*o(t-1);
     v = v + dv;
+    
     V(t) = v;
     
+    % fire spike if V > T
     if v > w^2/2
         o(t) = 1;
     end
     
+    % calculate prediction 
+    % NOTE: this is probably not the right way to calculate the prediction.
+    % Eqn. 1 in paper containt derivative of xhat, I just approximated that
+    % here with xhat(t) - xhat(t-1), which is probably not the best idea
     xh(t) = (w*o(t) + xh(t-1))/2;
 end
 
