b4a56e9d70d9dc33b0baa9ce58290a087becc730
gerdogan <gokererdogan@gmail.com>
2015-04-26 22:30:16 -0400

    Multi neuron case working: fig1_multi.m

--- fig1_multi.m
+++ fig1_multi.m
@@ -1,3 +1,5 @@
+clear 
+close all
 %% Input signal and shared parameters
 
 % simulation time (in mseconds)
@@ -13,14 +15,27 @@ t = 0:stepsize:T;
 x = ones(N, 1) * 0.01;
 xp = zeros(N, 1);
 
+% linear ramp input
+x = 0:(0.02/(N-1)):0.02;
+x = x';
+xp = [0; (x(2:N) - x(1:(N-1))) ./ stepsize];
+
+% random walk input
+x = abs(cumsum(randn(N, 1)));
+x = x * 0.0005;
+x = smooth(x, 50);
+% derivative
+xp = [0; (x(2:N) - x(1:(N-1))) / stepsize];
+
 %% Multiple neuron case
 
 % number of neurons
-K = 20;
+K = 100;
 
 % output weight
-w = [ones(K/2,1); ones(K/2,1)];
-w = 0.01 * w;
+w = [ones(K/2,1); -ones(K/2,1)];
+w = 0.0005 * w;
+w = randn(K,1) * 0.01;
 
 % regularization weight 
 mu = 1e-6;
@@ -37,24 +52,63 @@ xhat = zeros(N,1);
 reset_amount = (w*w' + mu*eye(K));
 
 for i = 2:N
-    dv = -V(:,i-1) + (w * (x(i-1) + xp(i-1))) - (reset_amount*o(:, i-1));
-    V(:,i) = V(:,i-1) + (stepsize * dv) + (randn(K,1) * 0);
-
-    % fire a single neuron, the one with maximum difference
-    diff = (V(:,i)) - diag(0.5 * (reset_amount));
-    % pick all the neurons with maximum voltage
-    while max(diff) > 0
-        maxk = find(diff==max(diff));
-        % pick one randomly and fire
-        k = maxk(randi(numel(maxk)));
-        o(k, i) = (1 / stepsize);
-        % update prediction
-        % WARNING: I am not sure if this is the right way to reset!
-        V(:,i) = V(:,i) - (stepsize * reset_amount(:,k));
-
-        diff = (V(:,i)) - diag(0.5 * (reset_amount));
-    end
+%     dv = -V(:,i-1) + (w * (x(i-1) + xp(i-1))) - (reset_amount*o(:, i-1));
+%     V(:,i) = V(:,i-1) + (stepsize * dv) + (randn(K,1) * 0);
     
+%     diff = (V(:,i)) - diag(0.5 * (reset_amount));
+%     if max(diff) > 0
+%         maxk = find(diff==max(diff));
+%         % pick one randomly and fire
+%         k = maxk(randi(numel(maxk)));
+%         o(k, i) = (1 / stepsize);
+%     end
+
+    dv = -V(:,i-1) + (w * (x(i-1) + xp(i-1)));
+    V(:,i) = V(:,i-1) + (stepsize * dv);
+    
+    for dt = 1:K
+        diff = V(:,i) - diag(0.5 * (reset_amount));
+        if max(diff) > 0
+            maxk = find(diff==max(diff));
+            % pick one randomly and fire
+            k = maxk(randi(numel(maxk)));
+            o(k, i) = (1 / stepsize);
+            % update prediction
+            V(:,i) = V(:,i) - (reset_amount(:,k));
+        end
+    end
+
+
+%     V(:,i) = V(:,i-1);
+%     
+%     for dt = 1:K
+%         diff = V(:,i) - diag(0.5 * (reset_amount));
+%         if max(diff) > 0
+%             maxk = find(diff==max(diff));
+%             % pick one randomly and fire
+%             k = maxk(randi(numel(maxk)));
+%             o(k, i) = (1 / stepsize);
+%         end
+%         % update prediction
+%         dv = -V(:,i) + (w * (x(i) + xp(i))) - (reset_amount*o(:, i));
+%         V(:,i) = V(:,i) + (stepsize * dv / K);
+%     end
+
+%     % fire a single neuron, the one with maximum difference
+%     diff = (V(:,i)) - diag(0.5 * (reset_amount));
+%     % pick all the neurons with maximum voltage
+%     while max(diff) > 0
+%         maxk = find(diff==max(diff));
+%         % pick one randomly and fire
+%         k = maxk(randi(numel(maxk)));
+%         o(k, i) = (1 / stepsize);
+%         % update prediction
+%         % WARNING: I am not sure if this is the right way to reset!
+%         V(:,i) = V(:,i) - (stepsize * reset_amount(:,k));
+% 
+%         diff = V(:,i) - diag(0.5 * (reset_amount));
+%     end
+%     
     % prediction from the first way of calculating voltage
     dxh = -xhat(i-1) + w'*o(:, i);
     xhat(i) = xhat(i-1) + (stepsize * dxh);
@@ -62,14 +116,12 @@ end
 
 
 figure(1)
-%plot(t, x)
-%plot(t, xhat)
-semilogy(t, x);
+plot(t, x)
 hold on
+plot(t, xhat)
+%semilogy(t, x);
 semilogy(t, xhat);
 % axis([0 T 1e-2 1]);
-%plot(t, V)
-% plot(t, xhat2)
 legend('x', 'xhat')
 
 figure(2)
